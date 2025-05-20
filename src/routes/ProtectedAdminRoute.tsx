e6b039fcd9663691139638f8f9556b4fbae8c7e9
Jose <josemelendezramos123@gmail.com>
2025-05-19 19:06:22 -0500

    Test: prueba de estado de carga para comprobar el rol del usuario.

--- src/routes/ProtectedAdminRoute.tsx
+++ src/routes/ProtectedAdminRoute.tsx
@@ -1,9 +1,18 @@
 import { useIsAdmin } from "@/providers/AuthProvider";
+import { LoaderCircle } from "lucide-react";
 import { Navigate, Outlet } from "react-router-dom";
 
 function ProtectedAdminRoute() {
     const isAuth = useIsAdmin();
-    if (isAuth === null) return null;
+    
+    if (isAuth === null) {
+        return (
+            <div className="w-screen h-screen grid place-items-center place-content-center">
+                <LoaderCircle className="animate-spin text-gray-600 size-5" />
+            </div>
+        );
+    };
+
     return isAuth ? <Outlet /> : <Navigate to="/acceso" />;
 }
 
