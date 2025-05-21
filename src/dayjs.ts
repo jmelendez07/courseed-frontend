b5d5a71b7267779d6d244f9a33762587d5e38c80
Jose <josemelendezramos123@gmail.com>
2025-05-21 12:49:36 -0500

    Fix: zona horaria actualizada

--- src/dayjs.ts
+++ src/dayjs.ts
@@ -1,8 +1,12 @@
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import relativeTime from "dayjs/plugin/relativeTime";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 import 'dayjs/locale/es';
 
+dayjs.extend(utc);
 dayjs.locale('es');
+dayjs.extend(timezone);
 dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
